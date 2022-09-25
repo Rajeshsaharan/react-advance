5c2f202dcd692198bdc116bbd18e7c5874af607f
rajeshsaharan <rajeshsharan1607@gmail.com>
2022-09-25 16:32:39 +0530

    props

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import React from "react";
-import {LearnJSX} from './components/LearnJSX' // named import 
+
+import FunctionProps from "./components/FunctionProps";
+import ClassProps from "./components/ClassProps";
 
 function App() {
   return <>
- <LearnJSX/>
+ <FunctionProps name = "rajesh"> 
+     {/* above example is props.name */}
+     <button>go back</button>  
+     <h2>you are not try to signin</h2>
+     {/* above example is for props.children */}
+ </FunctionProps>
+
+ <ClassProps name = "amit">
+  <button> go back</button>
+  <h2>you are not this person please log out</h2>
+ </ClassProps>
   </>;
 }
 
