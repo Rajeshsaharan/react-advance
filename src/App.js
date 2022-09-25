7b14a48410a10cc9aae691867c4275cfe9198e6b
rajeshsaharan <rajeshsharan1607@gmail.com>
2022-09-25 10:16:24 +0530

    components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 
+import React from "react";
+import FunctionalComponents from "./components/FunctionalComponent";
+import ClassComponent from "./components/ClassComponent";
 
 function App() {
   return <>
-  
+  <FunctionalComponents></FunctionalComponents>
+  <ClassComponent></ClassComponent>
   </>;
 }
 
---
+++ src/components/FunctionalComponent.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+
+function FunctionalComponents(){
+    return <h2>hello this is from FunctionalComponents</h2>
+}
+
+export default FunctionalComponents;
