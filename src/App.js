c158949ea5db92165299a776068951ac8e459dee
rajeshsaharan <rajeshsharan1607@gmail.com>
2022-09-25 17:10:52 +0530

    state

--- src/App.js
+++ src/App.js
@@ -1,21 +1,9 @@
 import React from "react";
-
-import FunctionProps from "./components/FunctionProps";
-import ClassProps from "./components/ClassProps";
+import Stateful from "./components/Stateful";
 
 function App() {
   return <>
- <FunctionProps name = "rajesh"> 
-     {/* above example is props.name */}
-     <button>go back</button>  
-     <h2>you are not try to signin</h2>
-     {/* above example is for props.children */}
- </FunctionProps>
-
- <ClassProps name = "amit">
-  <button> go back</button>
-  <h2>you are not this person please log out</h2>
- </ClassProps>
+     <Stateful></Stateful>
   </>;
 }
 
